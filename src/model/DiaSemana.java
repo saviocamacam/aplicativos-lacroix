19405a4812a59eab1385b46129c130d8df607f22
saviocamacam <saviocamacam@alunos.utfpr.edu.br>
2016-06-24 20:12:47 -0300

    Modificação do enum DiaSemana para conter o inteiro correspondente par reconhecimento do dia da semana.

--- src/model/DiaSemana.java
+++ src/model/DiaSemana.java
@@ -1,17 +1,26 @@
 package model;
 
 public enum DiaSemana {
-	SEGUNDA("segunda-feira"),
-	TERCA("terca-feira"),
-	QUARTA("quarta-feira"),
-	QUINTA("quinta-feira"),
-	SEXTA("sexta-feira"),
-	SABADO("sabado");
+	SEGUNDA("segunda-feira", 2),
+	TERCA("terca-feira", 3),
+	QUARTA("quarta-feira", 4),
+	QUINTA("quinta-feira", 5),
+	SEXTA("sexta-feira", 6),
+	SABADO("sabado", 7);
 	
 	String diaSemana;
+	int diaSemanaInt;
+	
+	DiaSemana(String diaSemana, int diaSemanaInt) {
+		this.diaSemana = diaSemana;
+		this.diaSemanaInt = diaSemanaInt;
+	}
 	
 	DiaSemana(String diaSemana) {
 		this.diaSemana = diaSemana;
 	}
-
+	
+	DiaSemana(int diaSemana) {
+		this.diaSemanaInt = diaSemana;
+	}
 }
