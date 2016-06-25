d41ff5089d6c38214a0772b732c9a004b768531a
Henrique Sousa <rickftime@gmail.com>
2016-06-25 20:09:05 -0300

    Modificações nos TableModels de Professor, Materia e Aula
    Modificado:
    - Implementada as funções para exibição e manipulação das tabelas

--- src/view/HorarioAulaTableModel.java
+++ src/view/HorarioAulaTableModel.java
@@ -1,25 +1,69 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.table.AbstractTableModel;
+
+import model.Aula;
+import model.Professor;
 
 public class HorarioAulaTableModel extends AbstractTableModel {
 
+	private static final int COL_DIA = 0;
+	private static final int COL_LOCAL = 1;
+	private static final int COL_INICIO = 2;
+	private static final int COL_FIM = 3;
+	
+	List<Aula> aulas;
+	
+	public HorarioAulaTableModel() {
+		this.aulas = new ArrayList<>();
+	}
+	
+	public HorarioAulaTableModel(List<Aula> aulas) {
+		this.aulas = aulas;
+	}
+	
+	public void addRow(Aula a){
+		this.aulas.add(a);
+		this.fireTableDataChanged();
+	}
+	
+	public void removeRow(int linha){
+		this.aulas.remove(linha);
+		this.fireTableRowsDeleted(linha, linha);
+	}
+
+	public Aula get(int linha){
+		return this.aulas.get(linha);
+	}
+	
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 4;
 	}
 
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return aulas.size();
 	}
 
 	@Override
-	public Object getValueAt(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Aula aula = aulas.get(rowIndex);
+		switch (columnIndex) {
+		case COL_DIA:
+			return aula.getDiaSemana();
+		case COL_LOCAL:
+			return aula.getLocal();
+		case COL_INICIO:
+			return aula.getHoraInicial();
+		case COL_FIM:
+			return aula.getHoraFinal();
+		default:
+			return "";
+		}
 	}
 
 }
--- src/view/ProfessorTableModel.java
+++ src/view/ProfessorTableModel.java
@@ -1,25 +1,63 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.table.AbstractTableModel;
 
+import model.Materia;
+import model.Professor;
+
 public class ProfessorTableModel extends AbstractTableModel {
+	
+	private static final int COL_NOME= 0;
+	private static final int COL_EMAIL= 1;
+	
+	List<Professor> professores;
+	
+	public ProfessorTableModel() {
+		this.professores = new ArrayList<>();
+	}
+	
+	public ProfessorTableModel(List<Professor> professores) {
+		this.professores = professores;
+	}
+	
+	public void addRow(Professor p){
+		this.professores.add(p);
+		this.fireTableDataChanged();
+	}
+	
+	public void removeRow(int linha){
+		this.professores.remove(linha);
+		this.fireTableRowsDeleted(linha, linha);
+	}
+
+	public Professor get(int linha){
+		return this.professores.get(linha);
+	}
 
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 2;
 	}
 
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return professores.size();
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		Professor professor = professores.get(rowIndex);
+		switch (columnIndex) {
+		case COL_NOME:
+			return professor.getNomeProfessor();
+		case COL_EMAIL:
+			return professor.getEmail();
+		default:
+			return "";
+		}
 	}
 
 }
