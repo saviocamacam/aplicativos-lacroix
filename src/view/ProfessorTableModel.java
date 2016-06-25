2448e55f271984ff9b0dbbe9fe302ef589c3f492
Henrique Sousa <rickftime@gmail.com>
2016-06-25 19:31:21 -0300

    Implementação das telas de Matéria e correções
    Modificado:
    -Botões da Coluna Principal agora são todos JToggleButtons
    
    Novo:
    -TableModel para Horario Aula
    -TableModel para Professor
    -TableModel para Materia

---
+++ src/view/ProfessorTableModel.java
@@ -0,0 +1,25 @@
+package view;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ProfessorTableModel extends AbstractTableModel {
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
